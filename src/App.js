23c379abaf7844eed0ea9e168f5b710d68191afd
RamonaEJSpence <ramonaejspence@gmail.com>
2020-02-03 16:21:00 -0500

    whoohoo, I didn't commit the whole time, whooops. This was a lot of fun to figure out, relatively easy and overall very simple. A great assignment to get aquainted with what I imagine is the very basics of components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props); //according to React Docs you should always pass props in for a class constructor, but I don't know why
+
+    this.state = {likes: 0} //this.state can only be assigned in the constructor, outside of the constructor, .setState() must be used
+    this.addLike = this.addLike.bind(this);
 }
 
+  addLike(event) { //this method is fired onClick of our button
+    this.setState((state, num) => { //.setState() can take an object or a function in this case
+      return {likes: state.likes + 1}
+
+    });
+
+  }
+
+      // render() {
+      //   const single = (this.state.likes = 1);
+      //   return (
+      //     <div className='App'>
+      //       <button className='like' onClick={this.addLike} type='button'><span>{this.state.likes} {single ? 'like':'likes'}</span></button>
+      //     </div> );
+
+
+  render() {  //this is a ???? so it takes some control flow logic.
+
+    if (this.state.likes === 1) {
+      return (
+      <div className="App">
+      <button className="like" onClick={this.addLike} type="button"><span>{this.state.likes} Like</span></button>
+      </div>
+    );} else
+  return (
+    <div className="App">
+    <button className="like" onClick={this.addLike} type="button"><span>{this.state.likes} Likes</span></button>
+    </div>
+  );  //I can definitely makes this control flow logic more simple and less code, but I don't know how quite yet.
+}
+}
 export default App;
