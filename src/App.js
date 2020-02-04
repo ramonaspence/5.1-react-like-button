fef5a10320f9671775ed234526f3f0b5c6df9176
RamonaEJSpence <ramonaejspence@gmail.com>
2020-02-04 09:06:58 -0500

    cleaned up and refactored

--- src/App.js
+++ src/App.js
@@ -8,36 +8,31 @@ class App extends Component {
     this.state = {likes: 0} //this.state can only be assigned in the constructor, outside of the constructor, .setState() must be used
     this.addLike = this.addLike.bind(this);
 }
-
   addLike(event) { //this method is fired onClick of our button
     this.setState((state, num) => { //.setState() can take an object or a function in this case
       return {likes: state.likes + 1}
-
     });
-
   }
 
-      // render() {
-      //   const single = (this.state.likes = 1);
-      //   return (
-      //     <div className='App'>
-      //       <button className='like' onClick={this.addLike} type='button'><span>{this.state.likes} {single ? 'like':'likes'}</span></button>
-      //     </div> );
+      render() {
+        // const single = (this.state.likes = 1);
+        return (
+          <div className='App'>
+            <button className='like' onClick={this.addLike} type='button'><span>{this.state.likes} {this.state.likes === 1 ? 'like':'likes'}</span></button>
+          </div> );
 
-
-  render() {  //this is a ???? so it takes some control flow logic.
-
-    if (this.state.likes === 1) {
-      return (
-      <div className="App">
-      <button className="like" onClick={this.addLike} type="button"><span>{this.state.likes} Like</span></button>
-      </div>
-    );} else
-  return (
-    <div className="App">
-    <button className="like" onClick={this.addLike} type="button"><span>{this.state.likes} Likes</span></button>
-    </div>
-  );  //I can definitely makes this control flow logic more simple and less code, but I don't know how quite yet.
-}
-}
+  // render() {  //this is a ???? so it takes some control flow logic.
+  //
+  //   if (this.state.likes === 1) {
+  //     return (
+  //     <div className="App">
+  //     <button className="like" onClick={this.addLike} type="button"><span>{this.state.likes} Like</span></button>
+  //     </div>
+  //   );} else
+  // return (
+  //   <div className="App">
+  //   <button className="like" onClick={this.addLike} type="button"><span>{this.state.likes} Likes</span></button>
+  //   </div>
+  // );  //I can definitely makes this control flow logic more simple and less code, but I don't know how quite yet.
+}}
 export default App;
